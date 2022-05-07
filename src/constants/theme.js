6912b3ff0477b23a9877fe59d4002898e5669185
Nahue Rodcast <nahuerodcast@gmail.com>
2022-05-07 16:19:33 -0300

    creating cart item component

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,6 +1,8 @@
-export const COLORS = {
+export const colors = {
   primaryColor: '#00BD9D',
   accentColor: '#54DEFD',
   textColor: '#212121',
   backgroundColor: '#FFFFFF',
+  errorMessage: 'red',
+  placeholder: '#9E9E9E',
 };
